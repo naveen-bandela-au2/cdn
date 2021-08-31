d2684e7488224e6d6b877f17cd16dd8d22b740a9
naveen-bandela-au2 <50129187+naveen-bandela-au2@users.noreply.github.com>
2021-09-01 05:08:52 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -128,3 +128,25 @@ function regUserToDB1(jsonData,token) {
         }
     });
 }
+const updateToken=()=> {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            saveToLocalStorage('user', JSON.stringify(user));
+            user.getIdToken().then(idToken => {
+                saveTokenToLocalStorage(idToken);
+            });
+            if (!user.emailVerified) {
+                saveToLocalStorage('emailVerified', false);
+            }
+        } else {
+            localStorage.removeItem('styldod-token');
+            localStorage.removeItem('userType');
+        }
+    });
+    
+    window.onload = function () {
+    updateToken();
+};
+
+
+
