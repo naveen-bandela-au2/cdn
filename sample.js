d310728768ee5f0261bcca8e63e6ad5c2e65e374
naveen-bandela-au2 <50129187+naveen-bandela-au2@users.noreply.github.com>
2021-09-01 02:54:33 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,44 @@
-console.log("helllo from github")
-console.log("bye from 98989898")
-console.log("hjhg")
+
+const userSignUP=()=>{
+if( validateUsername()&&validateUserType()&&validateEmail()&&validatePwd()&&aggreTerms()){
+    firebase.auth().createUserWithEmailAndPassword($("#userEmail").val().trim(), $("#userPassword").val().trim()).then(
+    auth => {
+    // $('.w-form-done').text('Success. Redirecting...').fadeIn();
+    // sendEmailVerification();
+    firebase.auth().currentUser.updateProfile({
+        displayName:$("#userName").val()                
+    }).then(function () {
+          firebase.auth().currentUser.getIdToken().then(idToken => {
+      localStorage.setItem("user", JSON.stringify(firebase.auth().currentUser))
+      saveTokenToLocalStorage(idToken)
+      
+        regUserToDB1({ userType: userType.val(), phone: authPhoneTxt1.val()},idToken).then((regPromise) => {
+        window.location = "user-dashboard";
+    });
+    
+      
+    
+        
+            /*regUserToDB(idToken).then(regPromise => {
+                if (!overrideAuth) {
+                    window.location = 'user-dashboard';
+                }
+            });*/
+        });
+    }).catch(function (error) {
+     
+        alert(error,'Something went wrong. Try again.');
+    });
+    },
+    error => {
+    //hideLoading();
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+        $('.w-form-fail').text('The password is too weak.').fadeIn().delay(3000).fadeOut();
+    } else {
+        $('.w-form-fail').text(errorMessage).fadeIn().delay(3000).fadeOut();
+    }
+    });
+    }
+}
